ae1e7960bffd2c3ffa6a6766d47c60bf5e8019f5
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-26 10:41:02 +0200

    Ejercicio 24 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -4,15 +4,34 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
+
     public static Queue<String> createQueue() {
-
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+        return queue;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+
+    public static void printAndEmptyQueue(Queue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println(queue.peek());
+            queue.poll();
+        }
     }
 
     public static void main(String[] args) {
+        Queue<String> queue = createQueue();
 
+        System.out.println("Elemento en la cima de la cola: " + queue.peek());
+
+        System.out.println("Imprimiendo y vaciando la cola:");
+        printAndEmptyQueue(queue);
+
+        if (queue.isEmpty()) {
+            System.out.println("La cola está vacía.");
+        }
     }
-
 }
